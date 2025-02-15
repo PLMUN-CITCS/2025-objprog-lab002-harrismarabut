05c0abbba0d16f54845959d294c28a10e95a80a3
harris <marabutharris_bsit@plmun.edu.ph>
2025-02-15 22:16:43 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 01

---
+++ VariablesAndCaseSensitivity.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+      
+        int studentAge = 15;
+
+       
+        double itemPrice = 29.99;
+
+       
+        String studentName = "Harris";
+
+
+        int counter = 10;
+        int Counter = 20; 
+
+        
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Item Price: $" + itemPrice);
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Counter: " + counter);
+        System.out.println("Counter (Different Case): " + Counter);
+    }
+}
