5ab83f82511de1ff38b48ec77105fc9ed292c9bf
harris <marabutharris_bsit@plmun.edu.ph>
2025-02-16 23:05:01 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 01

--- Main (1).java
+++ Main (1).java
@@ -3,21 +3,17 @@ public class Main {
       
         int studentAge = 15;
 
-       
         double itemPrice = 29.99;
 
-       
         String studentName = "Harris";
 
-
         int counter = 10;
-        int Counter = 20; 
+        int upperCounter = 20; 
 
-        
         System.out.println("Student Age: " + studentAge);
         System.out.println("Item Price: $" + itemPrice);
         System.out.println("Student Name: " + studentName);
         System.out.println("Counter: " + counter);
-        System.out.println("Counter (Different Case): " + Counter);
+        System.out.println("Upper Counter: " + upperCounter);
     }
 }
